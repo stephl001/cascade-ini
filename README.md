0dc5ae18dd8430e8d768b65a781ea12a8c8960fe
Stephane Larocque <stephl001@gmail.com>
2014-04-25 10:44:21 -0400

    Initial project creation with basic unit tests.

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# cascade-ini
+
+
---
+++ test/testconfig.js
@@ -0,0 +1,14 @@
+﻿var ini = require('../lib/cascade-ini.js');
+var fs = require('fs');
+
+exports.testOpenRoot = function(test){
+    test.expect(5);
+    var config = ini.parse(fs.readFileSync('./test/configs/test.ini', 'ascii'));
+
+    test.equal(config.A.prop1, 'value1');
+    test.equal(config.A.prop2, '134');
+    test.equal(config.B.prop2, 'value2');
+    test.equal(config.C.prop3, 'value3');
+    test.deepEqual(config.C.proparray, ['a','b','c']);
+    test.done();
+};
