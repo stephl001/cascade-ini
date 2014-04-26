c87937d1e72a72982e54ab0bb35f201904a3a0d2
Stephane Larocque <stephl001@gmail.com>
2014-04-25 23:53:42 -0400

    Added support for multi-level ini file parsing. Still need to handle missing file in a level.

--- test/testconfig.js
+++ test/testconfig.js
@@ -1,14 +1,59 @@
 ﻿var ini = require('../lib/cascade-ini.js');
 var fs = require('fs');
 
-exports.testOpenRoot = function(test){
+exports.testNoInheritance = function(test) {
     test.expect(5);
-    var config = ini.parse(fs.readFileSync('./test/configs/test.ini', 'ascii'));
+    var config = ini.parseFile('./test/configs/test.ini', function(err, config) {
+        test.equal(config.A.prop1, 'value1');
+        test.equal(config.A.prop2, '134');
+        test.equal(config.B.prop2, 'value2');
+        test.equal(config.C.prop3, 'value3');
+        test.deepEqual(config.C.proparray, ['a','b','c']);
+        test.done();
+    });
+};
 
-    test.equal(config.A.prop1, 'value1');
-    test.equal(config.A.prop2, '134');
-    test.equal(config.B.prop2, 'value2');
-    test.equal(config.C.prop3, 'value3');
-    test.deepEqual(config.C.proparray, ['a','b','c']);
-    test.done();
+exports.testSimpleInheritanceDefaultLevel = function(test) {
+    test.expect(4);
+    var config = ini.parseFile('./test/configs/child1/test.ini', function(err, config) {
+        test.equal(config.A.prop1, 'overriden1');
+        test.strictEqual(config.A.prop2, undefined);
+        test.strictEqual(config.B, undefined);
+        test.strictEqual(config.C, undefined);
+        test.done();
+    });
+};
+
+exports.testSimpleInheritanceInvalidLevel1 = function(test) {
+    test.expect(4);
+    var config = ini.parseFile('./test/configs/child1/test.ini', -1, function(err, config) {
+        test.equal(config.A.prop1, 'overriden1');
+        test.strictEqual(config.A.prop2, undefined);
+        test.strictEqual(config.B, undefined);
+        test.strictEqual(config.C, undefined);
+        test.done();
+    });
+};
+
+exports.testSimpleInheritanceInvalidLevel2 = function(test) {
+    test.expect(4);
+    var config = ini.parseFile('./test/configs/child1/test.ini', 'a', function(err, config) {
+        test.equal(config.A.prop1, 'overriden1');
+        test.strictEqual(config.A.prop2, undefined);
+        test.strictEqual(config.B, undefined);
+        test.strictEqual(config.C, undefined);
+        test.done();
+    });
+};
+
+exports.testSimpleInheritance2Levels = function(test) {
+    test.expect(5);
+    var config = ini.parseFile('./test/configs/child1/test.ini', 2, function(err, config) {
+        test.equal(config.A.prop1, 'overriden1');
+        test.equal(config.A.prop2, '134');
+        test.equal(config.B.prop2, 'value2');
+        test.equal(config.C.prop3, 'value3');
+        test.deepEqual(config.C.proparray, ['a','b','c']);
+        test.done();
+    });
 };
